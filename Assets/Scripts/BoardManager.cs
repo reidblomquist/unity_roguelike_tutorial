b3673e10cf891303673534a8f00422b35cdcdcc2
Reid Blomquist <reid@theprogrampdx.com>
2015-06-10 14:23:58 -0700

    formatting tweak

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -79,7 +79,7 @@ public class BoardManager : MonoBehaviour {
 		InitialiseList();
 		LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.maximum);
 		LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.maximum);
-		int enemyCount = (int)Mathf.Log (level,2f);
+		int enemyCount = (int)Mathf.Log (level, 2f);
 		LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount);
 		Instantiate(exit, new Vector3(columns -1, rows -1, 0f), Quaternion.identity);
 	}
